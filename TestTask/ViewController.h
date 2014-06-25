dffa41aba2ea265192316c20e12d77c3a224751c
Admin <admin@Admins-Mac.local>
2014-06-25 10:56:30 +0400

    Initial Commit

---
+++ TestTask/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestTask
+//
+//  Created by Admin on 25.06.14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
